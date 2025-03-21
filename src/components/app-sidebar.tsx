1785cf1d1e7fc6fdcafe0d9e945dec093ef8f611
tmns <methias@vertexprotocol.com>
2025-03-21 17:12:33 +0800

    Improve user auth button

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import {
   Sidebar,
   SidebarContent,
@@ -11,24 +10,48 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar";
 import { UserButton } from "@clerk/nextjs";
+import { ListTodo } from "lucide-react";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
       <SidebarHeader>
         <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              {/* Fixed height to prevent layout shift when loading user data. */}
-              <div className="h-9">
-                <UserButton />
-              </div>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
+          <div className="flex items-center gap-2 mb-4">
+            <ListTodo className="h-6 w-6" />
+            <h1 className="text-xl font-bold">Lists</h1>
+          </div>
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>Lists go here</SidebarContent>
-      <SidebarFooter></SidebarFooter>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem className="flex p-2 hover:bg-sidebar-accent">
+            <SidebarMenuButton size="lg" asChild>
+              <UserButton
+                showName
+                appearance={{
+                  elements: {
+                    rootBox: {
+                      width: "100%",
+                    },
+                    userButtonTrigger: {
+                      width: "100%",
+                      justifyContent: "flex-start",
+                    },
+                    userButtonBox: {
+                      flexDirection: "row-reverse",
+                    },
+                    userButtonOuterIdentifier: {
+                      color: "var(--sidebar-primary-foreground)",
+                    },
+                  },
+                }}
+              />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   );
